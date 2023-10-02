220f278c4d8897bd3a8cfb9fcd3c40ac8c5c5a26
Cury25 <curyfreitas@gmail.com>
2023-10-02 19:29:56 -0300

    testando api

---
+++ config.php
@@ -0,0 +1,10 @@
+
+<?php
+$db_host = 'localhost';
+$db_name = 'devesnotes';
+$db_user = 'root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host",$db_user, $db_pass);
+$array = [];
+
